f7b6fd78791739ecdaf7567daefa22d8426e8e32
Vranda Baheti <2022bit015@sggs.ac.in>
2024-02-01 22:28:44 +0530

    Java program for pattern printing '*'

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class pattern1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("enter no of rows:");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
