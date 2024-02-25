4513c3c21a86876f61c8e661b23767bfa213e748
Vranda Baheti <2022bit015@sggs.ac.in>
2024-02-25 20:39:13 +0530

    Program

---
+++ V2.java
@@ -0,0 +1,10 @@
+import java.io.File;
+class FilesandFoldersExample{
+	public static void main(String args[]){
+		FilesandFoldersExample FFE=new FilesandFoldersExample();
+		FFE.getFilesandFolders("/path");
+	}
+	public void getFilesandFolders(String path){
+		System.out.println(getname());
+	}
+}
