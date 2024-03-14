9f817f6fe1db6a7672656a818887db273c133df4
Vranda Baheti <2022bit015@sggs.ac.in>
2024-03-14 20:49:07 +0530

    Pattern printing

---
+++ pattern13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class pattern13
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,b;
+        System.out.println("enter no of rows:");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(b=1;b<i;b++)
+            System.out.print(" ");
+            for(j=1;j<=n+1-i;j++)
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
+
