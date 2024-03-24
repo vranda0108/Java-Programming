4cc3a0b67ecfa5dcc8a1c731002fd55fc3ae27d1
Vranda Baheti <2022bit015@sggs.ac.in>
2024-03-24 20:49:13 +0530

    Pattern printing

---
+++ pattern20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class pattern20
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,b,c=5;
+        System.out.println("enter no of rows:");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(b=1;b<=n-i;b++)
+            System.out.print(" ");
+            for(j=1;j<=(2*i)-1;j++)
+            System.out.print(c);
+            c=c-1;
+            System.out.println();
+        }
+    }
+}
+
